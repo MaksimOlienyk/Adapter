0c7a73fa308aba9a418c86669bdc31106ee74879
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 18:17:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿interface ITarget { void Request(); }
+class Adaptee { public void SpecificRequest() => Console.WriteLine("Specific request"); }
+
+class Adapter : ITarget
+{
+    private Adaptee _adaptee = new();
+    public void Request() => _adaptee.SpecificRequest();
+}
+
+class Program { static void Main() => new Adapter().Request(); }
